02b74794371f9ab07127d4b5ec1e3e0eb04f9c78
Gojo1281 <6dy2pqsmwp@privaterelay.appleid.com>
2025-12-28 16:25:36 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,39 @@
+NAME = input("Enter The Student: ")
+MATHS = int(input("Enter The Maths Marks: "))
+SCIENCE = int(input("Enter The Science Marks: "))
+ENGLISH = int(input("Enter The English Marks: "))
+TOTAL = MATHS + SCIENCE + ENGLISH
+PERCENTAGE = (TOTAL / 300) * 100
+GRADE = ""
+if PERCENTAGE >= 90:
+    GRADE = "A"
+elif PERCENTAGE >= 80:
+    GRADE = "B"
+elif PERCENTAGE >= 70:
+    GRADE = "C"
+elif PERCENTAGE >= 60:
+    GRADE = "D"
+else:
+    GRADE = "F"
+
+if PERCENTAGE >= 40:
+    RESULT = "PASS"
+else:
+    RESULT = "FAIL"
+    
+
+Subjects = ["Maths", "Science", "English"]
+maximum_marks = max(Subjects)
+minimum_marks = min(Subjects)
+print("-----Report Card-----")
+print("Name:", NAME)
+print("Maths Marks:", MATHS)
+print("Science Marks:", SCIENCE)
+print("English Marks:", ENGLISH)
+print("Total Marks:", TOTAL)
+print("Percentage:", PERCENTAGE)
+print("Grade:", GRADE)
+print("Result:", RESULT)
+print("Maximum Marks Subject:", maximum_marks)
+print("Minimum Marks Subject:", minimum_marks)
+    
